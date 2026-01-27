605489ff46616c63a3584f306d3525ab059d939e
GregoryElBajoury <injechta@gmail.com>
2026-01-27 05:51:01 +0100

    Fix : Migration state vers GCS terminée

--- main.tf
+++ main.tf
@@ -5,8 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
-}
 
+  # AJOUTE CE BLOC ICI
+  backend "gcs" {
+    bucket = "tf-state-mon-projet-devops-485603"
+    prefix = "terraform/state"
+  }
+}
 provider "google" {
   project = "mon-projet-devops-485603"
   region  = "us-central1"
@@ -36,4 +41,18 @@ resource "google_compute_instance" "vm_gratuite" {
     sudo systemctl start docker
     sudo systemctl enable docker
   EOT
+}
+
+# Création du Bucket pour le Terraform State
+resource "google_storage_bucket" "tf_state_bucket" {
+  name          = "tf-state-mon-projet-devops-485603" # Nom unique
+  location      = "US"                                # Inclus dans le Free Tier (ou EU)
+  force_destroy = false
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  public_access_prevention = "enforced"
 }
