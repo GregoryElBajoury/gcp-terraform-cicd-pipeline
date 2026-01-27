af2203e2cada18c7374e8c99ede9316aa2706a9e
GregoryElBajoury <injechta@gmail.com>
2026-01-27 06:18:50 +0100

    Add: Docker installation and port 80

--- main.tf
+++ main.tf
@@ -56,3 +56,49 @@ resource "google_storage_bucket" "tf_state_bucket" {
 
   public_access_prevention = "enforced"
 }
+
+# 1. On ouvre le port 80 dans le pare-feu
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+# 2. On modifie ton instance existante
+resource "google_compute_instance" "vm_gratuite" {
+  name         = "instance-docker-gratuite"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  tags         = ["http-server"] # Indispensable pour l'ouverture du port
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      # Garde l'IP externe actuelle : 34.70.68.41
+    }
+  }
+
+  # Script d'installation automatique
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y docker.io
+    systemctl start docker
+    systemctl enable docker
+    # Lance un petit conteneur de test pour prouver que ça marche
+    docker run -d -p 80:80 nginxdemos/hello
+  EOT
+}
