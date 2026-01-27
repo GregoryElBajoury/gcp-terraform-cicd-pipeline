543285beede70f5aed456c5cccb115c1c08b2dbf
GregoryElBajoury <injechta@gmail.com>
2026-01-27 05:06:35 +0100

    mise a jour ci cle gcp

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project     = "mon-projet-devops-485603" # À remplacer par l'ID de ton projet GCP
-  region      = "us-central1"
-  zone        = "us-central1-a"
+  project = "mon-projet-devops-485603"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
 resource "google_compute_instance" "vm_gratuite" {
   name         = "instance-docker-gratuite"
-  machine_type = "e2-micro" # Inclus dans le Free Tier
+  machine_type = "e2-micro"
 
   boot_disk {
     initialize_params {
@@ -26,11 +26,10 @@ resource "google_compute_instance" "vm_gratuite" {
   network_interface {
     network = "default"
     access_config {
-      # Donne une IP publique
+      # Alloue une IP publique
     }
   }
 
-  # Script pour installer Docker automatiquement au démarrage
   metadata_startup_script = <<-EOT
     sudo apt-get update
     sudo apt-get install -y docker.io
